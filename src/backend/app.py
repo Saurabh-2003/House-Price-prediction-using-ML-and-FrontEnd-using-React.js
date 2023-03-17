9116d27ace08503ec8a1b6b4ee97dab35fd47d95
learngit-prog <saurabhthapliyal20011682@gmail.com>
2023-03-17 21:24:56 +0530

    Made the model with 80+ % accuracy and used Gradient Boosting regression

--- src/backend/app.py
+++ src/backend/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
 # Load the model
-model = pickle.load(open('model.pkl', 'rb'))
+model = pickle.load(open('gbr.pkl', 'rb'))
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -17,7 +17,7 @@ def predict():
             return jsonify({'error': 'Invalid request format'})
 
         # Make prediction using the model.
-        prediction = model.predict([[float(data['floors']), int(data['bedrooms']), float(data['bathrooms']), int(data['yr_built'])]])[0]
+        prediction = model.predict([[int(data['bedrooms']), float(data['bathrooms']), float(data['sqft_living']), float(data['sqft_lot']), float(data['floors']), int(data['condition']),float(data['sqft_basement']), int(data['yr_built']), int(data['yr_renovated']), float(data['lat']), float(data['long'])]])[0]
 
         # Convert the prediction to a string and return it.
         output = {'prediction': str(prediction)}
